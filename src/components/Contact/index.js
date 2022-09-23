b7472362e75d386a8f0f4700c8074fbe6b0da04e
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-09-23 17:36:42 -0500

    form data submit functionality complete

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState }from 'react';
 
 const ContactForm = () => {
+  
+  // setter and getter for useState, sets initial values to empty strings
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  // destructure formState into the form elements
+  const { name, email, message } = formState;
 
-  // JSX
+  // syncs the internal state of the component formState with the user input from the DOM
+  // onChange event listener (in name input) ensures that handleChange fires whenever a keystroke is typed in the field
+  const handleChange = (e) => {
+    setFormState({...formState, [e.target.name]: e.target.value })
+  }
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(formState);
+    }
+    
+  // renders the form element
   return (
     <section>
       <h1>
         Contact me
       </h1>
-      <form id="contact-form">
+      <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" defaultValue={name} onChange={handleChange} name="name" />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" defaultValue={email} onChange={handleChange} name="email" />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
         </div>
         <button type="submit">Submit</button>
       </form>
